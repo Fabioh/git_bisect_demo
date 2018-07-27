a79b5cda90ba29cf10cd662ae83dd7203819c55e
Fábio Henrique Gabriele <fabiohenriqueg@gmail.com>
2018-07-26 23:08:42 -0300

    - expandindo os casop de testes

--- GitBisectDemo.Tests/UnitTest1.cs
+++ GitBisectDemo.Tests/UnitTest1.cs
@@ -25,5 +25,40 @@ namespace GitBisectDemo.Tests
 			var v = Fatorial.CalculaFatorial(3);
 			v.ShouldBe(3 * 2);
 		}
+
+		[Fact]
+		public void Fat4()
+		{
+			var v = Fatorial.CalculaFatorial(4);
+			v.ShouldBe(4 * 3 * 2);
+		}
+
+		[Fact]
+		public void Fat5()
+		{
+			var v = Fatorial.CalculaFatorial(5);
+			v.ShouldBe(5 * 4 * 3 * 2);
+		}
+
+		[Fact]
+		public void Fat8()
+		{
+			var v = Fatorial.CalculaFatorial(8);
+			v.ShouldBe(1 * 2 * 3 * 4 * 5 * 6 * 7 * 8);
+		}
+
+		[Fact]
+		public void Fat10()
+		{
+			var v = Fatorial.CalculaFatorial(10);
+			v.ShouldBe(1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10);
+		}
+
+		[Fact]
+		public void Fat13()
+		{
+			var v = Fatorial.CalculaFatorial(12);
+			v.ShouldBe(1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10 * 11 * 12);
+		}
 	}
 }
